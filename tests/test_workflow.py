2be99111b70091b5ccfbfbab3f3780c5a6ad62d2
Austin Hovland <austin@sansterbioanalytics.com>
2023-02-19 15:21:41 +0000

    update workflow fixture

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -7,18 +7,11 @@ def snakemake_workflow(tmp_path) -> int:
     """Test that the Snakemake workflow runs without errors."""
     print('Starting Snakemake workflow test')
     # Construct the Snakemake command
-    snakemake_cmd = 'snakemake --use-conda --directory tests'
+    snakemake_cmd = 'snakemake --cores 4 --use-conda --directory tests --snakefile workflow/Snakefile --configfile tests/config/config.yaml'
     # Specify any additional arguments to the Snakemake command here, for example:
-    snakemake_cmd += ' --cores 4'
-    # Copy the test data to a temporary directory
-    command = f'cp -r tests/data {tmp_path}'
-    # Change the working directory to the temporary directory
-    command += f' && cd {tmp_path}/data'
-    
+    snakemake_cmd += ''
+   
     # Run the commands using subprocess.run
-    print(f'Running command: {command}')
-    process = subprocess.run(f'{command}', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    print('Finished running setup commands')
     print(f'Running command: {snakemake_cmd}')
     process = subprocess.run(f'{snakemake_cmd}', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     print('Finished running Snakemake workflow')

1518ec77976ee981c57755eb99e34be42efe0ca6
Austin Hovland <austin@sansterbioanalytics.com>
2023-03-06 01:18:08 +0000

    start attempting to fix workflow pytest

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,29 +1,29 @@
 import subprocess
 import pytest
-import os
+from pathlib import Path
 
 @pytest.fixture(scope="session")
-def snakemake_workflow(tmp_path_factory) -> int:
+def snakemake_workflow(tmp_path_factory: Path) -> subprocess.CompletedProcess:
     """Test that the Snakemake workflow runs without errors."""
     # Setup testing directory
-    test_dir = tmp_path_factory.mktemp("test_workflow")
+    test_dir = tmp_path_factory.mktemp("workflow_test") / "kallisto-sleuth"
     # Copy the workflow to the testing directory
-    os.system(f'cp -r workflow {test_dir}')
-    os.system(f'cp -r tests {test_dir}')
+    subprocess.run([f'pwd'])
+    subprocess.run([f'cp -r workflow {test_dir}/workflow'])
+    subprocess.run([f'cp -r tests {test_dir}/tests'])
     # Construct the Snakemake command
-    snakemake_cmd = f'snakemake --cores all --use-conda --directory {test_dir}/tests --snakefile {test_dir}/workflow/Snakefile --configfile {test_dir}/tests/config/config.yaml'
-    # Specify any additional arguments to the Snakemake command here, for example:
+    snakemake_cmd = f'snakemake --cores all --use-conda --directory tests --snakefile workflow/Snakefile --configfile tests/config/config.yaml'
+    # Specify any additional arguments to the Snakemake command to test individual modules, for example:
     snakemake_cmd += ''
 
     # Run the commands using subprocess.run
     print(f'Running command: {snakemake_cmd}')
-    process = subprocess.run(f'{snakemake_cmd}', stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE, shell=True, cwd=test_dir)
+    completed_process = subprocess.run([snakemake_cmd], cwd=test_dir, shell=True, check=True)
     print('Finished running Snakemake workflow')
-    return process.returncode
+    return completed_process
 
 
 def test_workflow_execution(snakemake_workflow):
     """Test that the Snakemake workflow runs without errors."""
     print('Starting Snakemake workflow test')
-    assert snakemake_workflow == 0
+    assert snakemake_workflow.returncode == 0
